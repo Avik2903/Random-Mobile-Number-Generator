6db539eaedaf48a041873d0631876c8be24bd5b9
Avik2903 <76903360+Avik2903@users.noreply.github.com>
2022-09-19 17:44:36 +0530

    removed spaces

--- mobilenumbergenerator.py
+++ mobilenumbergenerator.py
@@ -12,12 +12,9 @@ window.geometry("600x200")
 window.config(bg="#F39C12")
 window.resizable(width=False,height=False)
 window.title('Random Mobile Number Generator')
- 
- 
+
 l1 = tk.Label(text="Random Mobile Number Generator",font=("Arial",20),bg="Black",fg="White")
- 
 b1 = tk.Button(text="Click on me to generate a mobile number",font=("Arial",15),bg="#A3E4D7",command=generate_number)
- 
 l2 = tk.Label(bg="#F39C12",font=("Arial",30),text="")
  
 l1.place(x=100,y=20)
